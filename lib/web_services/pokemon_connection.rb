0b62a14af85170fa24cae01410a3017ce4a0ea36
Daniel Penaloza <danielpenalozatj@gmail.com>
2024-11-22 15:11:07 -0800

    refactorin and adding specs

--- lib/web_services/pokemon_connection.rb
+++ lib/web_services/pokemon_connection.rb
@@ -2,7 +2,7 @@ module WebServices
   class PokemonConnection
     def client(read_timeout = 30)
       Faraday.new(url: 'https://pokeapi.co/api/v2/') do |conn|
-        conn.options.open_timeout = 10
+        conn.options.open_timeout = 30
         conn.options.read_timeout = read_timeout
         conn.request :json
         conn.response :logger, nil, { headers: false, bodies: false, errors: false }
@@ -13,30 +13,8 @@ module WebServices
 
     def get_pokemon(pokemon_name:)
       response = client.get("pokemon/#{pokemon_name}")
-      return create_pokemon(response.body)
     rescue Faraday::Error => e
       { 'error' => e }
-    end
-
-    private
-
-    def create_pokemon(body)
-      {
-        'name': body['name'],
-        'weight': body['weight'],
-        'types': type(body['types']),
-        'stats': stats(body['stats'])
-      }
-    end
-
-    def stats(stats)
-      stats.each_with_object([]) do |stat, array|
-        array << "#{stat.dig('stat', 'name')}: #{stat['base_stat']}"
-      end
-    end
-
-    def type(types)
-      types = types.map { |type| type.dig('type', 'name') }
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,8 +36,9 @@ end
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr_cassettes'
-  c.hook_into :webmock
   c.configure_rspec_metadata!
+  c.hook_into :faraday
+
   c.default_cassette_options = {
     record: :new_episodes
   }
